d8d9c6b81916f66f1a7120cb4fa8c35cdc7afe3d
bissaliev <bissaliev21@gmail.com>
2024-12-15 08:24:54 +0400

    рефакторинг модулей

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,18 +7,29 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
-                            ShoppingCart, Tag)
 from .filters import RecipeFilter, SearchIngredientsFilter
 from .paginators import CustomPaginator
 from .permissions import IsAuthorOrReadOnly
-from .serializers import (FavoriteShoppingSerializer, IngredientSerializer,
-                          RecipeCreateSerializer, RecipeReadSerializer,
-                          TagSerializer)
+from .serializers import (
+    FavoriteShoppingSerializer,
+    IngredientSerializer,
+    RecipeCreateSerializer,
+    RecipeReadSerializer,
+    TagSerializer,
+)
+from recipes.models import (
+    Favorite,
+    Ingredient,
+    IngredientRecipe,
+    Recipe,
+    ShoppingCart,
+    Tag,
+)
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    """ Класс представления рецептов. """
+    """Класс представления рецептов."""
+
     queryset = Recipe.objects.all()
     serializer_class = RecipeCreateSerializer
     pagination_class = CustomPaginator
@@ -27,73 +38,78 @@ class RecipeViewSet(viewsets.ModelViewSet):
     filterset_class = RecipeFilter
 
     def get_serializer_class(self):
-        if self.request.method == 'GET':
+        if self.request.method == "GET":
             return RecipeReadSerializer
         return RecipeCreateSerializer
 
     def __func_post_delete(self, model, request, pk):
         recipe = get_object_or_404(Recipe, pk=pk)
         user = request.user
-        if request.method == 'POST':
+        if request.method == "POST":
             model.objects.get_or_create(user=user, recipe=recipe)
             serializer = FavoriteShoppingSerializer(recipe)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         model.objects.filter(user=user, recipe=recipe).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     @action(
-        detail=True, methods=['POST', 'DELETE'], url_path='favorite',
-        permission_classes=(IsAuthenticated,)
+        detail=True,
+        methods=["POST", "DELETE"],
+        url_path="favorite",
+        permission_classes=(IsAuthenticated,),
     )
     def favorite(self, request, pk=None):
-        """ Добавление или удаление рецепта из избранного. """
+        """Добавление или удаление рецепта из избранного."""
         return self.__func_post_delete(Favorite, request, pk)
 
     @action(
-        detail=True, methods=['POST', 'DELETE'],
-        permission_classes=(IsAuthenticated,)
+        detail=True,
+        methods=["POST", "DELETE"],
+        permission_classes=(IsAuthenticated,),
     )
     def shopping_cart(self, request, pk=None):
         return self.__func_post_delete(ShoppingCart, request, pk)
 
     @action(
-        detail=False, methods=['GET'], permission_classes=(IsAuthenticated,)
+        detail=False, methods=["GET"], permission_classes=(IsAuthenticated,)
     )
     def download_shopping_cart(self, request):
         ingredient_list = "Cписок покупок:"
-        ingredients = IngredientRecipe.objects.filter(
-            recipe__shopping__user=request.user
-        ).values(
-            'ingredient__name', 'ingredient__measurement_unit'
-        ).annotate(sum_amount=Sum('amount'))
+        ingredients = (
+            IngredientRecipe.objects.filter(
+                recipe__shopping__user=request.user
+            )
+            .values("ingredient__name", "ingredient__measurement_unit")
+            .annotate(sum_amount=Sum("amount"))
+        )
         for num, i in enumerate(ingredients):
             ingredient_list += (
                 f'\n{i["ingredient__name"]} - '
                 f'{i["sum_amount"]} {i["ingredient__measurement_unit"]}'
             )
             if num < ingredients.count() - 1:
-                ingredient_list += ', '
-        file = 'shopping_list'
+                ingredient_list += ", "
+        file = "shopping_list"
         response = HttpResponse(
-            ingredient_list, 'Content-Type: application/pdf'
+            ingredient_list, "Content-Type: application/pdf"
         )
-        response['Content-Disposition'] = f'attachment; filename="{file}.pdf"'
+        response["Content-Disposition"] = f'attachment; filename="{file}.pdf"'
         return response
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
-    """ Класс представления тегов. """
+    """Класс представления тегов."""
+
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = (AllowAny,)
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
-    """ Класс представления ингредиентов. """
+    """Класс представления ингредиентов."""
+
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     permission_classes = (AllowAny,)
     filter_backends = (SearchIngredientsFilter,)
-    search_fields = ('^name',)
+    search_fields = ("^name",)
--- backend/recipes/management/commands/load_ingridients.py
+++ backend/recipes/management/commands/load_ingridients.py
@@ -3,21 +3,19 @@ import csv
 from django.conf import settings
 from django.core.management import BaseCommand
 
-from ...models import Ingredient
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         data_path = settings.BASE_DIR
         with open(
-            f'{data_path}/data/ingredients.csv',
-            'r',
-            encoding='utf-8'
+            f"{data_path}/data/ingredients.csv", encoding="utf-8"
         ) as file:
             reader = csv.DictReader(
-                file, fieldnames=['name', 'measurement_unit']
+                file, fieldnames=["name", "measurement_unit"]
             )
             Ingredient.objects.bulk_create(
                 [Ingredient(**data) for data in reader]
             )
-        self.stdout.write(self.style.SUCCESS('Все ингредиенты загружены!'))
+        self.stdout.write(self.style.SUCCESS("Все ингредиенты загружены!"))
