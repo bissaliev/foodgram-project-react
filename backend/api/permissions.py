41f902a7005d1144b499fb41894a793c405d55b2
bissaliev <bissaliev21@gmail.com>
2024-12-29 08:46:15 +0400

    Исправил работу permission.IsAuthorOrReadOnly

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,6 +2,9 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsAuthorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS or request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         """Изменения доступны только автору."""
         return request.method in SAFE_METHODS or obj.author == request.user
@@ -9,7 +12,6 @@ class IsAuthorOrReadOnly(BasePermission):
 
 class UserOrAdminOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        print(obj)
         return (
             request.method in SAFE_METHODS
             or request.user.is_authenticated
